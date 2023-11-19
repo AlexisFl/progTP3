eeb0a401c1d39b7c095fb81fac2323e47c00b971
af303746 <alexis_falconnet@etu.u-bourgogne.fr>
2023-11-19 17:13:02 +0100

    Question 4.1

---
+++ src/app/page.tsx
@@ -0,0 +1,26 @@
+import { BreadCrumbs, SectionContainer } from "tp-kit/components";
+import { PRODUCTS_CATEGORY_DATA } from "tp-kit/data";
+import { ProductList } from "../components/product-list";
+import { Metadata } from "next";
+import {getCategories} from "../utils/database";
+import prisma from "../utils/prisma";
+
+export const metadata:Metadata = {
+  title: `Page d’accueil - Starbucks`,
+  description: "Commandez de délicieuses boissons préparées avec soin par nos baristas"
+}
+
+
+export default async function Home() {
+    const categories = await getCategories();
+  return (<SectionContainer>
+    <BreadCrumbs items={[
+      {
+        label: "Accueil",
+        url: "/"
+      }
+    ]} />
+
+    <ProductList categories={categories} showFilters />
+  </SectionContainer>);
+}
